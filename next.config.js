03d5fac90b6ab8a675d84c4cc3a296448e6eef9f
Aqsa-Abdullah090 <aqsa.abdull@zimogroup.org>
2025-06-13 19:41:24 +0500

    fix issues

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
    
-  };
+//   };
   
-  export default nextConfig;
+//   export default nextConfig;
   
 
 
 
   
-// const withPWA = require('next-pwa')({
-//   dest: 'public',
-//   disable: process.env.NODE_ENV === 'development',
-//   register: true,
-//   // scope: '/app',
-//   // sw: 'service-worker.js',
-//   //...
-// })
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  // scope: '/app',
+  // sw: 'service-worker.js',
+  //...
+})
 
-// module.exports = withPWA({
-//   // next.js config
-// })
+module.exports = withPWA({
+  // next.js config
+})
